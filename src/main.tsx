362bc2b143614c372e68c60b3489aa362e239610
Арсений <arsenyukrainsky@yandex.ru>
2024-03-12 18:54:36 +0300

    Шрифты и сброс стилей

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/templates/App.tsx'
+import App from './components/pages/App.tsx'
+import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
